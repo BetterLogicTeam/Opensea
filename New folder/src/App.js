9f6382100d0d6f7a85c7d1481acda8e85a4fcdcb
BetterLogicTeam <102576102+BetterLogicTeam@users.noreply.github.com>
2023-01-03 09:39:03 +0500

    push

---
+++ New folder/src/App.js
@@ -0,0 +1,127 @@
+
+import './App.css';
+import Ecsnft from './components/first_slider/Ecsnft';
+import Frist_open from './components/Frist_open/Frist_open';
+import Navtr from './components/Navbar/Navtr';
+import Simple_two_cards from './components/Simple_two_cards/Simple_two_cards';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+import Drops from './components/Drops/Drops';
+import Resposive_Navbar from "./components/Responsive_Navbar/Resposive_Navbar"
+import View_tab from './components/View_tab/View_tab';
+import Mainhome from './components/Mainhome/Mainhome';
+import Footer from './components/Footer/Footer';
+import Nextdg from "./components/Next_dg/Nextdg"
+import Nextdrop from './components/Next_dg/Nextdrop';
+import FAQ from './components/Next_dg/FAQ';
+import Minnt from './components/Next_dg/Minnt';
+import Resrources_nft from './components/Resrources_nft/Resrources_nft';
+import R_nft_card from './components/R_nft_card/R_nft_card';
+import R_bottom_links from './components/R_bottom_links/R_bottom_links';
+import R_links_help from './components/R_links_help/R_links_help';
+import Hh_hlep_card from './components/Hh_help_card/Hh_hlep_card';
+// import hh_help_card_full from './components/Hh_help_card_full/Hh_help_card_full';
+import Hh_help_card_full from './components/Hh_help_card_full/Hh_help_card_full';
+import Help_permote from './components/Help_permote/Help_permote';
+import Resources_header from "./components/Resources_header/Resources_header"
+import Meet_opensea from "./components/Meet_opensea/Meet_opensea"
+import R_web3 from './components/R_web3/R_web3';
+import R_blockchain from './components/R_blockchain/R_blockchain';
+import R_watch from './components/R_watch/R_watch';
+import R_insities from './components/R_insites/R_insities';
+// import Resource_full_page from './components/Resource_full_page/Resource_full_page';
+import Resource_full_page from "./components/Resource_full_page/Resource_full_page"
+import R_bottom_links_full_page from './components/R_bottom_links_full_page/R_bottom_links_full_page';
+import R_full_tex_page from './components/R_full_tex_page/R_full_tex_page';
+import Wallet_offcanvas from './components/Wallet_offcanvas/Wallet_offcanvas';
+import R_partner_full from './components/R_partner_full/R_partner_full';
+import Explore_main from './components/Exploar_main/Explore_main';
+import Art_hover from './components/Art_hover/Art_hover';
+import Explore_all_nft from './components/Explore_all_nft/Explore_all_nft';
+import Colloctibles_hover from './components/colloctibles_hover/Colloctibles_hover';
+import Domine_hover from './components/Domine_hover/Domine_hover';
+
+import Music_hover from './components/Music_hover/Music_hover';
+import Photography_hover from './components/Photography_hover/Photography_hover';
+import Sport_hover from './components/Sport_hover/Sport_hover';
+import Trading_hover from './components/Trading_hover/Trading_hover';
+import Utilityhover from './components/Utility_hover/Utilityhover';
+import Virtual_hover from './components/Virtual_hover/Virtual_hover';
+import Activity from './components/Activity/Activity';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Drops/> */}
+      {/* <Drop_slider/> */}
+      {/* <Minnt/> */}
+      {/* <Nextdrop/> */}
+      {/* <Navtr/> */}
+      {/* <Frist_open/> */}
+      {/* <Ecsnft/> */}
+      {/* <Simple_two_cards/> */}
+      <BrowserRouter>
+        <Navtr/>
+        <Resposive_Navbar/>
+        <Routes>
+        <Route path="/" element={<Mainhome />} />
+        <Route path="/Drop" element={< Drops/>} />
+        <Route path="/View_tab" element={< View_tab/>} />
+        <Route path="/resources_main_page" element={<Resource_full_page/>} />
+        <Route path="/resource_main_help" element={<R_bottom_links_full_page/>} />
+        <Route path="/R_full_tax_page" element={<R_full_tex_page/>} />
+        <Route path="/R_full_partner" element={<R_partner_full/>} />
+        <Route path="/Explore_main" element={<Explore_main/>} />
+        <Route path="/Art_hover" element={<Art_hover/>} />
+        <Route path="/Explore_all_nft" element={<Explore_all_nft/>} />
+        <Route path="/Colloctibles_hover" element={<Colloctibles_hover/>} />
+        <Route path="/Domine_hover" element={<Domine_hover/>} />
+        <Route path="/Music_hover" element={<Music_hover/>} />
+        <Route path="/Photography_hover" element={<Photography_hover/>} />
+        <Route path="/Sport_hover" element={<Sport_hover/>} />
+        <Route path="/Trading_hover" element={<Trading_hover/>} />
+        <Route path="/Utilityhover" element={<Utilityhover/>} />
+        <Route path="/Virtual_hover" element={<Virtual_hover/>} />
+        <Route path="/Nextdg" element={<Nextdg/>} />
+        <Route path="/Activity" element={<Activity/>} />
+        
+
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+      {/*  */}
+    
+      {/* <Wallet_offcanvas/> */}
+      <>
+       {/* <Resrources_nft/> */}
+   
+      
+      
+      {/* 
+      
+      //
+      */}
+      </>
+      {/* <Resrources_nft/> */}
+      {/* <Resources_header/>
+      <Meet_opensea/>
+      <R_nft_card/> 
+      <R_web3/>
+      <R_blockchain/>
+      <R_watch/>
+      <R_insities/>
+      <R_bottom_links/>  */}  
+
+    </div>
+  );
+}
+
+export default App;
---
+++ New folder/src/components/Frist_open/Frist_open.jsx
@@ -0,0 +1,256 @@
+// import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./stylishmain.css"
+import React, { useRef, useState } from "react";
+import "swiper/css";
+import { Autoplay, Navigation } from "swiper";
+import "swiper/css/navigation";
+import Frist_card from "../Frist_card/Frist_card";
+import ninja from "../Assets/ninja.avif"
+import ninja1 from "../Assets/da4525d7-9eb6-4e0b-9056-09aea4d1a33d.avif"
+import ninja2 from "../Assets/riDbUm07GjqPcnK3Ll7r7y4b6lHNNQS4AHtghn46WXmic0JnOG87XgaUWkTmb1iYS80oyaYfx6kDSCGd62wXH7Lp2REIwfeekwYVtWA.avif"
+import ninja3 from "../Assets/0b8ea573-c2b0-431d-8538-4034ad57966a.avif"
+import ninja4 from "../Assets/4541c8d0-0ed1-4a25-a93d-8bb55dc76653.avif"
+import ninja5 from "../Assets/88bb0626-fabf-43e7-b3fa-99e2aeef8a46.avif"
+function Frist_open() {
+  return (
+    <div className="container-fluid backgrid">
+      <div className='py-4'><h1 className='text-center pb-3  top_margin'>Explore, collect, and sell NFTs</h1></div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          300: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+        }}
+        loop={true}
+        navigation={true}
+        Autoplay={true}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja1} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja2} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja3} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja4} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+          <Frist_card img1={ninja5} h1="KPR" h2="Floor:0,24 ETH" />
+        </SwiperSlide>
+
+      </Swiper>
+      {/* <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+          loop={true}
+          navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/9dcc.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/dcc.avif" alt="" className='second_pic bg-white' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>9dcc ITERATION-02 Proof of</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+           
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/krap.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/krpin.avif" alt="" className='second_pic' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>Krapopolis Krap Chickens</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/metro.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/metin.avif" alt="" className='second_pic' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>Metroverse Genesis</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/neo.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/ntin.avif" alt="" className='second_pic' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>Neo Tokyo Citizens</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/super.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/spin.avif" alt="" className='second_pic' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>Supercute World</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+        </SwiperSlide>
+        <SwiperSlide className='silde befr'>
+        <div className='main_box'>
+                     <div className='fst_pic_uper'>
+                        <span className='frsts_pan'>
+                        <img src="./images/coolcat.avif" alt="" className='frst_pic'/>
+                        </span>
+                    </div>
+
+                     <div className='lower d-flex'>
+
+                        <div className='lower_pic_uper'>
+                           <span className='scnds_spn'>
+                              <img src="./images/coolin.avif" alt="" className='second_pic' />
+                           </span>
+                        </div>
+
+                        <div className='heading_uper'>
+                            <div className='undr_heading'>
+                                <div className='heading'>Cool Cats Football Club</div>
+                            </div>
+                        </div>
+            
+                     </div>
+
+                </div>
+        </SwiperSlide>
+        
+      </Swiper> */}
+      
+
+    </div>
+
+  )
+}
+
+export default Frist_open
