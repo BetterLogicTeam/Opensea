ab0c1692860b4fc9ece9ebaf7ae4ab7abada7f75
betterlogics <108340548+betterlogics@users.noreply.github.com>
2023-04-11 13:08:33 +0500

    updated version with first slider

--- src/components/Frist_open/Frist_open.jsx
+++ src/components/Frist_open/Frist_open.jsx
@@ -20,7 +20,7 @@ import './First_openn.css'
 function Frist_open() {
   return (
     <div className="container-fluid backgrid ">
-      <div className=' ps_fixed'><h1 className='text-center pb-3  top_margin'>Explore, collect, and sell NFTs</h1></div>
+      {/* <div className=' ps_fixed'><h1 className='text-center pb-3  top_margin'>Explore, collect, and sell NFTs</h1></div> */}
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -45,7 +45,7 @@ function Frist_open() {
         navigation={true}
         Autoplay={true}
         modules={[Autoplay, Navigation]}
-        className="mySwiper"
+        className="mySwiper mt-5"
       >
         <SwiperSlide className='silde befr'>
         <Link to="/Profile_collection" className="text-decoration-none"><div className="uper">
@@ -59,10 +59,10 @@ function Frist_open() {
                               <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
                             </div>
                             <div className="row main_time_box mt-4 ">
-                              <div className="col-lg-4 col-md-2 time">
-                                <button className="btn btn-lg text-white minto">MINTING NOW</button>
+                              <div className="col-lg-5 col-md-2 time">
+                                <button className="btn w-100 text-white minto">MINTING NOW</button>
                               </div>
-                              <div className="col-lg-8 col-md-2 view gYb ">
+                              <div className="col-lg-7 col-md-2 view gYb ">
                                 <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
                                 <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
                               </div>
@@ -73,7 +73,29 @@ function Frist_open() {
           {/* <Link to="/Profile_collection"><Frist_card img1={ninja} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
         </SwiperSlide>
         <SwiperSlide className='silde befr'>
-          <Link to="/Profile_collection"><Frist_card img1={ninja1} h1="KPR" h2="Floor:0,24 ETH" /></Link>
+          {/* <Link to="/Profile_collection"><Frist_card img1={ninja1} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
+          <Link to="/Profile_collection" className="text-decoration-none"><div className="uper">
+                        <video src="./images/Gemesis - Drop.mp4" autoPlay loop playsinline muted className="aga_in parko"></video>
+                        <div className="row for_position_main text-white">
+                          <div className="for_wrt col-lg-11 col-md-9">
+                            {/* <div className="pic_first text-start"><img src={collpic} alt="" className="cool_img" /></div> */}
+                            <div className="in_between mt-4 text-start">
+                              <h3 className="fw-bolder lfm">inBetweeners x Dolce&Gabbana Drip<MdVerified className='ms-1' /></h3>
+                              <h6 className="fw-bold lfm">By&nbsp;ItsMeGianPiero<MdVerified className='ms-1' /></h6>
+                              <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
+                            </div>
+                            <div className="row main_time_box mt-4 ">
+                              <div className="col-lg-5 col-md-2 time">
+                                <button className="btn btn-lg text-white minto">MINTING NOW</button>
+                              </div>
+                              <div className="col-lg-7 col-md-2 view gYb ">
+                                <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
+                                <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div></Link>
         </SwiperSlide>
         <SwiperSlide className='silde befr'>
           {/* <Link to="/Profile_collection"><Frist_card img1={ninja2} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
@@ -88,10 +110,10 @@ function Frist_open() {
                               <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
                             </div>
                             <div className="row main_time_box mt-4 ">
-                              <div className="col-lg-4 col-md-2 time">
+                              <div className="col-lg-5 col-md-2 time">
                                 <button className="btn btn-lg text-white minto">MINTING NOW</button>
                               </div>
-                              <div className="col-lg-8 col-md-2 view gYb ">
+                              <div className="col-lg-7 col-md-2 view gYb ">
                                 <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
                                 <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
                               </div>
@@ -101,7 +123,29 @@ function Frist_open() {
                       </div></Link>
         </SwiperSlide>
         <SwiperSlide className='silde befr'>
-          <Link to="/Profile_collection"><Frist_card img1={ninja3} h1="KPR" h2="Floor:0,24 ETH" /></Link>
+          {/* <Link to="/Profile_collection"><Frist_card img1={ninja3} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
+          <Link to="/Profile_collection" className="text-decoration-none"><div className="uper">
+                        <video src="./images/Gemesis - Drop.mp4" autoPlay loop playsinline muted className="aga_in parko"></video>
+                        <div className="row for_position_main text-white">
+                          <div className="for_wrt col-lg-11 col-md-9">
+                            {/* <div className="pic_first text-start"><img src={collpic} alt="" className="cool_img" /></div> */}
+                            <div className="in_between mt-4 text-start">
+                              <h3 className="fw-bolder lfm">inBetweeners x Dolce&Gabbana Drip<MdVerified className='ms-1' /></h3>
+                              <h6 className="fw-bold lfm">By&nbsp;ItsMeGianPiero<MdVerified className='ms-1' /></h6>
+                              <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
+                            </div>
+                            <div className="row main_time_box mt-4 ">
+                              <div className="col-lg-5 col-md-2 time">
+                                <button className="btn btn-lg text-white minto">MINTING NOW</button>
+                              </div>
+                              <div className="col-lg-7 col-md-2 view gYb ">
+                                <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
+                                <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div></Link>
         </SwiperSlide>
         <SwiperSlide className='silde befr'>
           {/* <Link to="/Profile_collection"><Frist_card img1={ninja4} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
@@ -116,10 +160,10 @@ function Frist_open() {
                               <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
                             </div>
                             <div className="row main_time_box mt-4 ">
-                              <div className="col-lg-4 col-md-2 time">
+                              <div className="col-lg-5 col-md-2 time">
                                 <button className="btn btn-lg text-white minto">MINTING NOW</button>
                               </div>
-                              <div className="col-lg-8 col-md-2 view gYb ">
+                              <div className="col-lg-7 col-md-2 view gYb ">
                                 <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
                                 <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
                               </div>
@@ -129,7 +173,29 @@ function Frist_open() {
                       </div></Link>
         </SwiperSlide>
         <SwiperSlide className='silde befr'>
-          <Link to="/Profile_collection"><Frist_card img1={ninja5} h1="KPR" h2="Floor:0,24 ETH" /></Link>
+          {/* <Link to="/Profile_collection"><Frist_card img1={ninja5} h1="KPR" h2="Floor:0,24 ETH" /></Link> */}
+          <Link to="/Profile_collection" className="text-decoration-none"><div className="uper">
+                        <video src="./images/Gemesis - Drop.mp4" autoPlay loop playsinline muted className="aga_in parko"></video>
+                        <div className="row for_position_main text-white">
+                          <div className="for_wrt col-lg-11 col-md-9">
+                            {/* <div className="pic_first text-start"><img src={collpic} alt="" className="cool_img" /></div> */}
+                            <div className="in_between mt-4 text-start">
+                              <h3 className="fw-bolder lfm">inBetweeners x Dolce&Gabbana Drip<MdVerified className='ms-1' /></h3>
+                              <h6 className="fw-bold lfm">By&nbsp;ItsMeGianPiero<MdVerified className='ms-1' /></h6>
+                              <h6 className="fw-bold lfm">2,000 items. 0.88 ETH</h6>
+                            </div>
+                            <div className="row main_time_box mt-4 ">
+                              <div className="col-lg-5 col-md-2 time">
+                                <button className="btn btn-lg text-white minto">MINTING NOW</button>
+                              </div>
+                              <div className="col-lg-7 col-md-2 view gYb ">
+                                <div className="bell_1 me-3"><AiOutlineBell className='fs-4 ' /></div>
+                                <div className="bell px-4 py-3"><span className="fw-bold">View drop</span></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div></Link>
         </SwiperSlide>
 
       </Swiper>
